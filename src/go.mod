1c3ca1b065f7615659ffa83af84449f70a55ac6f
RyuseiNomi <kanontonto1010@gmail.com>
2020-10-21 18:33:33 +0900

    create scraping process from hokurikucar by using goquery

--- src/go.mod
+++ src/go.mod
@@ -1,4 +1,7 @@
-require github.com/aws/aws-lambda-go v1.13.3
+require (
+	github.com/PuerkitoBio/goquery v1.6.0
+	github.com/aws/aws-lambda-go v1.13.3
+)
 
 module github.com/hokurikucar/PeriodTweetTrigger/src
 
--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,7 +1,41 @@
 package handler
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const hokurikuCarURL = "https://hokurikucar.com/"
+const articleSelectorPath = "article > div > h2 > a"
 
 func Handler() {
-	fmt.Printf("aaa")
+
+	log.Println("started")
+	// Webサイトへのリクエスト
+	res, err := http.Get(hokurikuCarURL)
+	if err != nil {
+		log.Fatal("Http Request Error: %s", err)
+	}
+	defer res.Body.Close()
+
+	log.Println(res)
+	if res.StatusCode != 200 {
+		log.Fatal("Response from website is not 200: %d %s", res.StatusCode, res.Status)
+	}
+
+	// HTMLドキュメントの取得
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal("Load HTML Document Error: %s", err)
+	}
+	log.Println(doc)
+	doc.Find(articleSelectorPath).Each(func(i int, s *goquery.Selection) {
+		// 取得できた記事１つ１つに対する処理
+		title := s.Text()
+		url, _ := s.Attr("href")
+		log.Println(title)
+		log.Println(url)
+	})
 }
