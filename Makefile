0553b09b978a1155ae8d245eb8b698e215bb722b
RyuseiNomi <kanontonto1010@gmail.com>
2020-10-27 13:13:00 +0900

    create tweet process

--- Makefile
+++ Makefile
@@ -5,4 +5,5 @@ build:
 
 exec-dev:
 	sam build; sam local invoke PeriodTweetTrigger \
-		--skip-pull-image
+		--skip-pull-image \
+		--env-vars env.json
--- src/tweet/tweet.go
+++ src/tweet/tweet.go
@@ -1,21 +1,13 @@
 package tweet
 
 import (
-	"log"
 	"os"
 
 	"github.com/ChimeraCoder/anaconda"
-	"github.com/joho/godotenv"
 )
 
 // Tweet APIにて連携済みのtwitterアカウントにツイートを送信する
 func Tweet(text string, url string) error {
-	// 環境変数の読み込み
-	// TODO build後のディレクトリ構成を調べてenvファイルを読み込む
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Load Environment Error: %v", err)
-	}
-
 	tweetContent := text + "\n" + url
 	api := getCredential()
 	_, err := api.PostTweet(tweetContent, nil)
@@ -27,5 +19,10 @@ func Tweet(text string, url string) error {
 
 // getCredential twitterアカウントのAPIを取得する
 func getCredential() *anaconda.TwitterApi {
-	return anaconda.NewTwitterApi(os.Getenv("TWITTER_ACCESS_TOKEN"), os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"))
+	return anaconda.NewTwitterApiWithCredentials(
+		os.Getenv("TWITTER_ACCESS_TOKEN"),
+		os.Getenv("TWITTER_ACCESS_TOKEN_SECRET"),
+		os.Getenv("TWITTER_API_KEY"),
+		os.Getenv("TWITTER_SECRET_KEY"),
+	)
 }
